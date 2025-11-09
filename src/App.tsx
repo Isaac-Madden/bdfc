f50c92dba36e1348f2231311214521eb0b835915
Isaac Madden <isaacmadden@hotmail.co.uk>
2025-11-09 20:44:16 +0000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import bdfcLogo from './assets/logo.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={bdfcLogo} className="logo react" alt="React logo" />
+      <h1>Bradford Disability Football Club</h1>
+    </>
+  )
+}
+
+export default App
