5fa73c75c2ea4b3f12eaff53b7dc58bf611b3c42
Isaac Madden <isaacmadden@hotmail.co.uk>
2025-12-22 12:59:56 +0000

    footer updates as per client

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Styles/Footer.css';
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebookF, FaEnvelope } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -8,30 +8,28 @@ const Footer: React.FC = () => {
   return (
     <footer className="Footer">
       
-      <div className="Content"> 
-
-        <div className="Contact">
-          <h3>Get In Touch</h3>
-          <p>Email: <a href="mailto:bradforddisabilityfc@gmail.com">bradforddisabilityfc@gmail.com</a></p>
-          <p>Phone: (01274) 123 456</p>
-          <p>Bradford, West Yorkshire</p>
-        </div>
-
-        <div className="Social">
-          <h3>Connect</h3>
-          <div className="SocialIcons">
-            <a href="https://www.facebook.com/BradfordDFC/" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-              <span role="img" aria-label="Facebook Icon"><FaFacebookF /></span>
+      <div className="Content">
+        <h3>Contact us</h3>
+        <ul className="SocialIcons">
+          <li>
+            <a 
+              href="mailto:bradforddisabilityfc@gmail.com" 
+              aria-label="Send us an Email"
+            >
+              <FaEnvelope aria-hidden="true" />
             </a>
-            <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-              <span role="img" aria-label="Twitter Icon"><FaTwitter /></span>
+          </li>
+          <li>
+            <a 
+              href="https://www.facebook.com/BradfordDFC/" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              aria-label="Follow us on Facebook"
+            >
+              <FaFacebookF aria-hidden="true" />
             </a>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-              <span role="img" aria-label="Instagram Icon"><FaInstagram /></span>
-            </a>
-          </div>
-        </div>
-
+          </li>
+        </ul>
       </div>
 
       <div className="Copyright">
