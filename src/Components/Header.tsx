2194d0fb24defae664e6652d9225e820daa1d637
Isaac Madden <isaacmadden@hotmail.co.uk>
2025-12-23 15:08:19 +0000

    logo update

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Styles/Header.css'
-import bcfcNewLogo from '../assets/bcfc-temp-logo.png'
 import donateIcon from '../assets/donate-icon-temp-2.png'
+import bdfcLogo from '../assets/bdfc_club_logo.png'
 
 const donationLink = "https://cafdonate.cafonline.org/9155?fbclid=Iwb21leAOHB9djbGNrA4cH0WV4dG4DYWVtAjExAHNydGMGYXBwX2lkDDM1MDY4NTUzMTcyOAhjYWxsc2l0ZQIyNQABHs-qwSeLD29W4dYRI0y8hy0evtFx_dyzBibBwJZzSZq2o-RPPrurziTyZsy9_aem__Fxw55ie619e9Vf87f_I_A#!/DonationDetails"
 
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
 
   return (
     <div className='Header'>
-      <img src={bcfcNewLogo} alt="Bradford Disability Club Badge" className="logoIcon"/>
+      <img src={bdfcLogo} alt="Bradford Disability Club Badge" className="logoIcon"/>
       <h1>Bradford Disability Football Club</h1>
       <a
         className="HeaderDonate"
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../Styles/NavBar.css'; // Make sure the path is correct
-import menuSVG from '../assets/menu.svg'; // Import the hamburger menu SVG  
+import '../Styles/NavBar.css'; 
+import { FaBars } from 'react-icons/fa';
 
 interface NavLink {
     name: string;
@@ -35,7 +35,8 @@ const Navbar: React.FC = () => {
     return (
         <nav className="main-nav">
             <button className="hamburger-icon" onClick={toggleMenu} aria-expanded={isOpen} aria-label="Toggle navigation menu">
-                <img src={menuSVG} alt="Menu icon" className="menu-icon-svg" />
+                {/* <img src={menuSVG} alt="Menu icon" className="menu-icon-svg" /> */}
+                <FaBars aria-hidden="true" />
                 <span className="icon-text">MENU</span>
             </button>
 
