892d135e84bfbfdd6774145846abbe2f8a20d6e9
Isaac Madden <isaacmadden@hotmail.co.uk>
2025-12-23 15:18:12 +0000

    logo tweaks

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Styles/Header.css'
 import donateIcon from '../assets/donate-icon-temp-2.png'
 import bdfcLogo from '../assets/bdfc_club_logo.png'
+import { Link } from 'react-router-dom';
 
 const donationLink = "https://cafdonate.cafonline.org/9155?fbclid=Iwb21leAOHB9djbGNrA4cH0WV4dG4DYWVtAjExAHNydGMGYXBwX2lkDDM1MDY4NTUzMTcyOAhjYWxsc2l0ZQIyNQABHs-qwSeLD29W4dYRI0y8hy0evtFx_dyzBibBwJZzSZq2o-RPPrurziTyZsy9_aem__Fxw55ie619e9Vf87f_I_A#!/DonationDetails"
 
@@ -9,7 +10,9 @@ const Header: React.FC = () => {
 
   return (
     <div className='Header'>
-      <img src={bdfcLogo} alt="Bradford Disability Club Badge" className="logoIcon"/>
+      <Link to="/" className="homeLink" aria-label="Go to Bradford Disability Football Club homepage">
+        <img src={bdfcLogo} alt="Bradford Disability Club Badge" className="logoIcon"/>
+      </Link>
       <h1>Bradford Disability Football Club</h1>
       <a
         className="HeaderDonate"
