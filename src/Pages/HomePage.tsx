e71ea60f4d4a99a3e42f346c9773009f485c78bb
Isaac Madden <isaacmadden@hotmail.co.uk>
2026-01-25 13:18:25 +0000

    homepage content start

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import '../Styles/PageStyles/HomePage.css';
 
+import bdfc_pic from '../assets/bdfc_pic.png';
+
 const HomePage: React.FC = () => {
   return (
-    <div className="HomePage">   
-        <p>Hey i'm the Home page</p>
+    <div className="HomePage">
+        <div className="text-section">
+          <h2>Welcome!</h2>
+          <p>
+            Founded in 2001, 
+            Bradford Disability FC is an award-winning football club dedicated to providing 
+            people of all ages and disabilities with the opportunity to play the sport they love.
+          </p>
+          <p>
+            We offer a safe, welcoming and inclusive environment where members can improve their fitness, build confidence 
+            and make lifelong friends.
+          </p>
+          <p>
+            With a vibrant social calendar for members and families, 
+            we are more than just a club—we’re a community.
+          </p>
+          <p>
+            Join us at a training session today!
+          </p>
+        </div>
+        
+        <div className="image-section">
+          <img 
+            src={bdfc_pic}
+            alt="Football action shot" 
+          />
+        </div>
     </div>
   )
 }
+
 export default HomePage;
+
+
+
