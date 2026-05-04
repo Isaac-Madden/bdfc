6318883a1385c4e48210f6ab37e27918b2c75c18
Isaac Madden <isaacmadden@hotmail.co.uk>
2026-05-04 19:20:10 +0100

    about us page content

--- src/Pages/AboutUsPage.tsx
+++ src/Pages/AboutUsPage.tsx
@@ -4,7 +4,27 @@ import '../Styles/PageStyles/AboutUsPage.css';
 const AboutUsPage: React.FC = () => {
     return (
     <div className="AboutUsPage">   
-        <p>Hey i'm the AboutUsPage</p>
+         <div className="text-section">
+          <h2>About Us!</h2>
+          <p>
+            Celebrating its 25th anniversary, Bradford Disability Football Club (BDFC) is the largest disability football club in the area, supporting more than 100 members and transforming lives through inclusive sport.  
+          </p>
+          <p>
+            Founded in 2001 by Paul Squires and Richard Carney, the club began with just eight players and a clear mission: to ensure that people with disabilities in Bradford had access to meaningful football opportunities.  
+          </p>
+          <p>
+            Today, it is a thriving, volunteer-led charity supported by 26 dedicated volunteers, delivering weekly training sessions and providing opportunities to compete in local, national and international tournaments.
+          </p>
+          <p>
+            The club creates clear player pathways, supporting members to attend Mencap trials and progress into further competitive opportunities. Just as importantly, it nurtures leadership and personal development. Through a members’ committee, players contribute ideas and help shape the future of the organisation, building confidence and a sense of ownership.
+          </p>
+          <p>
+            A player-centred approach ensures a safe and family-focused environment where everyone feels welcome and can develop at their own pace. The club’s impact is profound, with participants reporting that they feel healthier, happier and more confident.
+          </p>
+          <p>
+            For many families, BDFC is more than a football club—it is a second family and a community where friendships flourish and everyone truly belongs.
+          </p>
+        </div>
     </div>
     )
 }
